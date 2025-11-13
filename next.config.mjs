25ace063489cb4e280e376b64b6476dfe838a3cf
Amir Alam <sameer72135@gmail.com>
2025-11-13 18:57:24 +0530

    next config file update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // ESLint errors ko ignore karega deployment mein
+  },
+  images: {
+    unoptimized: true, // Image warnings fix
+  },
+};
 
 export default nextConfig;
